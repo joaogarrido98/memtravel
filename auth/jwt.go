ae78472538a2deb3de5996ddb287657c5966b6b5
Joao Garrido <joao.melo.garrido@gmail.com>
2024-06-29 20:28:56 +0100

    fix: pass jwt token struct as response instead of just valid We can now access the claims on verify token

--- auth/jwt.go
+++ auth/jwt.go
@@ -23,14 +23,14 @@ func CreateToken(userID string) (string, error) {
 }
 
 // VerifyToken verifies a jwt token for a specific user
-func VerifyToken(signedToken string) (bool, error) {
+func VerifyToken(signedToken string) (*jwt.Token, error) {
 	token, err := jwt.Parse(signedToken, func(token *jwt.Token) (interface{}, error) {
 		return configs.Envs.JWTSecret, nil
 	})
 
 	if err != nil {
-		return false, err
+		return &jwt.Token{}, err
 	}
 
-	return token.Valid, err
+	return token, err
 }

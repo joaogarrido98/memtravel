4964022531ea89ba4603a7574d7cafe9798abdfa
Joao Garrido <joao.melo.garrido@gmail.com>
2024-07-13 19:06:20 +0100

    feat: add new serverresponse and change password types

--- types/types.go
+++ types/types.go
@@ -1,9 +1,21 @@
 package types
 
+// ServerResponse holds the generic type for all responses in
+type ServerResponse struct {
+	Response string      `json:"rs"`
+	Message  string      `json:"ms"`
+	Data     interface{} `json:"dt,omitempty"`
+}
+
 type User struct {
-	UserID   string `json:"userid,omitempty"`
+	UserID   string `json:"id,omitempty"`
 	Email    string `json:"email,omitempty"`
 	Password string `json:"password,omitempty"`
 	Phone    string `json:"phone,omitempty"`
 	Active   bool   `json:"active,omitempty"`
+}
+
+type ChangePassword struct {
+	OldPassword string `json:"op,omitempty"`
+	NewPassword string `json:"np,omitempty"`
 }
